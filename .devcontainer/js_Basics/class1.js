c115e45eeebec8e86a0296b3015236240c3b951e
Gopal-Mishra-06 <130341696+Gopal-Mishra-06@users.noreply.github.com>
2023-10-15 11:35:05 +0000

    Data types

---
+++ .devcontainer/js_Basics/class1.js
@@ -0,0 +1,7 @@
+const accountId = 12345
+let accountEmail = "gopalmishra@gmail.com"
+var accountPassword ="121212"
+accountCity = "Telangana"
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ .devcontainer/js_Basics/dataTypes.js
@@ -0,0 +1,33 @@
+// alert(3+3) // we are using Note.js not brower
+
+console.log(3+3); 
+console.log("mishra");
+
+let name = "Mishra"
+let age = 23
+let isLoggedIn = true 
+
+// number 
+
+let a = 12 
+console.log(typeof a)
+
+// string 
+
+let b = "asfg"
+console.log(b);
+
+// boolean 
+
+let c = true 
+console.log(c);
+
+//bigint
+
+// null
+
+// undefined 
+
+//symbol 
+
+// object
