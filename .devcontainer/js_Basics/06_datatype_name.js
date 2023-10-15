826e11c6bb863de4dd134bb7fe229caa0ac228b4
Gopal-Mishra-06 <130341696+Gopal-Mishra-06@users.noreply.github.com>
2023-10-15 15:17:51 +0000

    Datatypes name

---
+++ .devcontainer/js_Basics/06_datatype_name.js
@@ -0,0 +1,19 @@
+// there are two types of datatypes
+
+// 1) primitive & 2) Non- primitive
+
+// a ) primitive have 7 types
+
+1) Number
+2) String
+3) Boolean
+4) null
+5) undefined
+6) BigInt
+7) Symbol
+
+// b) Non- primitive are 3 types
+
+1) Array
+2) Object
+3) function
