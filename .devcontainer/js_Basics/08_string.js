db1212526737ae6cd5c3305b757f8f377ea135ae
Gopal-Mishra-06 <130341696+Gopal-Mishra-06@users.noreply.github.com>
2023-10-17 11:27:58 +0000

    string methods

---
+++ .devcontainer/js_Basics/08_string.js
@@ -0,0 +1,88 @@
+// // ` ${} `
+
+// let a = "mishra"
+// console.log(`My name is ${a}`)
+
+// let d = a.toUpperCase()
+// console.log(d)
+
+// let c = d.toLowerCase()
+// console.log(c)
+
+// a.trim()
+// console.log(a)
+
+// console.log(a.length);
+
+// console.log(a.charAt(2))   // if we want to check the char value
+// console.log(a.indexOf('i')); 
+
+// let z = "Hello gopal mishra how are you doing nowadays"
+// let newstring = z.substring(2,8)
+// console.log(newstring);
+
+
+
+// console.log(z.length);
+
+// let newslice = z.slice(-8,4)
+// console.log(newslice);
+
+// let a = "Gopal Mishra"
+// let b = a.length
+// console.log(b)
+
+
+// let c = a.charAt(3)
+// console.log(c)
+
+// let d = a.slice(0,3)
+// console.log(d)
+
+// let e = a.slice(-12,-4)
+// console.log(e)
+
+
+// let x = "gopal-mishra-06"
+// let y = x.split('-')
+// console.log(y);
+
+// let m = ['hai, ','How','are', 'you','doing']
+// let n = m.join('-')
+// console.log(n);
+
+// let g = "hai gopal how are you doing this days how"
+// let h = g.replace("gopal",'mishra')
+// console.log(h);
+
+// let j = g.search('hai')
+// console.log(j)
+
+// let k = g.indexOf('how')
+// console.log(k)
+
+// let l = g.lastIndexOf('how')
+// console.log(l);
+
+// let asd = g.startsWith("ha")
+// console.log(asd);
+
+// let asdf = g.endsWith("how")
+// console.log(asdf);
+
+// let sa = "----jfgnfjngdf   dfsgdf----"
+// let sad = sa.trim('-')
+// console.log(sad);
+
+ let mn = "i am gopal mishra. I'm from Hyderabad,Telangana"
+ let rt = mn.includes('mishra')
+console.log(rt);
+
+
+let vb = mn.includes('mishra', 9)
+console.log(vb)
+
+let a = '1'
+let b = '3'
+let c = a.concat(b)
+console.log(c)
