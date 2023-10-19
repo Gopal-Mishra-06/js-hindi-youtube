5cf5942048404f50a7cebdf362e49a9e74bc3824
Gopal-Mishra-06 <130341696+Gopal-Mishra-06@users.noreply.github.com>
2023-10-19 05:40:48 +0000

    array methods

---
+++ .devcontainer/js_Basics/11_Array_methods.js
@@ -0,0 +1,42 @@
+// let a = [1,2,3,4,5,5,67]
+// let b = a 
+// b[2]="mishra"
+// a.push(45)
+// console.log(a)
+
+// a.pop()
+// console.log(a)
+
+// a.pop()
+// console.log(a)
+
+// a.shift()
+
+// console.log(a)
+
+// a.unshift('gopal mishra')
+// console.log(a);
+
+// let x = a.includes(10)
+// console.log(x);
+
+// let c = a.slice(0,3)
+// console.log(c);
+
+// let d = a.splice(0,3)
+// console.log(a)
+// console.log(d);
+
+// let x = ['1','3','4']
+// let y = x.join('-')
+// console.log(y)
+
+let x = [1,4,6,3,6,8]
+// let y = x.sort()
+// console.log(y);
+
+let y = ['a','c']
+
+let z = x.concat(y)
+console.log(z);
+ 
